130e0903e9807954e52f36a6df3cb64bb179aeb3
yude <i@yude.jp>
2024-08-31 10:05:16 +0900

    feat: Add parameter `order_by` @ /api/players

--- api/player.go
+++ api/player.go
@@ -15,10 +15,15 @@ func GetPlayers(db *gorm.DB, m map[string]string) fiber.Map {
 	sort, sort_ok := m["sort"]
 	offset, offset_ok := m["offset"]
 	limit, limit_ok := m["limit"]
-	if !(filter_ok && sort_ok && offset_ok && limit_ok) {
+	if !(filter_ok && sort_ok && offset_ok) {
 		return fiber.Map{
 			"error": "Required parameters not provided",
 		}
+	}
+
+	order_by, order_by_ok := m["order_by"]
+	if !(order_by_ok) {
+		order_by = "UUID"
 	}
 
 	// Parse parameters
@@ -35,20 +40,31 @@ func GetPlayers(db *gorm.DB, m map[string]string) fiber.Map {
 		}
 	}
 
-	l, err := strconv.Atoi(limit)
-	if err != nil || l < 0 {
-		return fiber.Map{
-			"error": "Parameter `limit` is not valid",
+	var l int
+	if limit_ok {
+		l, err := strconv.Atoi(limit)
+		if err != nil || l < 0 {
+			return fiber.Map{
+				"error": "Parameter `limit` is not valid",
+			}
 		}
-	}
-	if l > 50 {
+		if l > 50 {
+			l = 50
+		}
+	} else {
 		l = 50
+	}
+
+	if !(order_by == "uuid" || order_by == "experience_points" || order_by == "currency" || order_by == "total_build_blocks" || order_by == "total_destroy_blocks" || order_by == "total_mob_kills" || order_by == "play_time") {
+		return fiber.Map{
+			"error": "Parameter `order_by` is not valid",
+		}
 	}
 
 	var players []structs.Player
 	db.
 		Where("uuid LIKE ?", "%"+filter+"%").
-		Order(fmt.Sprintf("uuid %v", sort)).
+		Order(fmt.Sprintf("%v %v", order_by, sort)).
 		Limit(l).
 		Offset(o).
 		Find(&players)
