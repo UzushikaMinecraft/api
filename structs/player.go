3134b7d09afbc61980692f14b1ac40d36f99e850
iamtakagi <github@iamtakagi.net>
2024-09-03 12:53:26 +0900

    fix: big.Int

--- structs/player.go
+++ structs/player.go
@@ -7,7 +7,7 @@ type Player struct {
 	uuid                 string    `gorm:"type:char(36);unique;not null"`
 	initial_login_date   time.Time `gorm:"not null"`
 	last_login_date      time.Time
-	total_play_time      bigint  `gorm:"default:0"`
+	total_play_time      big.Int  `gorm:"default:0"`
 	experience           float64 `gorm:"default:0.0"`
 	currency             int     `gorm:"default:0"`
 	total_build_blocks   int     `gorm:"default:0"`
