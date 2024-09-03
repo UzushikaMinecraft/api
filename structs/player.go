566278b70280038ce60199954e967b6283d559be
iamtakagi <github@iamtakagi.net>
2024-09-03 13:00:17 +0900

    fix: type of total_play_time is int64

--- structs/player.go
+++ structs/player.go
@@ -6,11 +6,11 @@ type Player struct {
 	id                   int       `gorm:"primaryKey;autoIncrement"`
 	uuid                 string    `gorm:"type:char(36);unique;not null"`
 	initial_login_date   time.Time `gorm:"not null"`
-	last_login_date      time.Time
-	total_play_time      big.Int  `gorm:"default:0"`
-	experience           float64 `gorm:"default:0.0"`
-	currency             int     `gorm:"default:0"`
-	total_build_blocks   int     `gorm:"default:0"`
-	total_destroy_blocks int     `gorm:"default:0"`
-	total_mob_kills      int     `gorm:"default:0"`
+	last_login_date      time.Time `gorm:"not null"`
+	total_play_time      int64     `gorm:"default:0"`
+	experience           float64   `gorm:"default:0.0"`
+	currency             int       `gorm:"default:0"`
+	total_build_blocks   int       `gorm:"default:0"`
+	total_destroy_blocks int       `gorm:"default:0"`
+	total_mob_kills      int       `gorm:"default:0"`
 }
