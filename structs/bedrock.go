71291341c74b67ebebe890f335f91d99efb0872a
yude <i@yude.jp>
2024-09-05 20:31:27 +0900

    fix: fix struct tags

--- structs/bedrock.go
+++ structs/bedrock.go
@@ -1,9 +1,9 @@
 package structs
 
 type Bedrock struct {
-	ID   int    `gorm:"column:id;primaryKey;autoIncrement"json:"id"`
-	FUID string `gorm:"column:fuid;type:char(36);unique;not null"json:"fuid"`
-	XUID string `gorm:"column:xuid;type:char(36);unique;not null"json:"xuid"`
+	ID   int    `gorm:"column:id; primaryKey; autoIncrement" json:"id"`
+	FUID string `gorm:"column:fuid; type:char(36); unique;not null" json:"fuid"`
+	XUID string `gorm:"column:xuid; type:char(36); unique;not null" json:"xuid"`
 }
 
 func (Bedrock) TableName() string {
