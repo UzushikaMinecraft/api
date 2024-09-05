edbcf0f90bc8cf942ec72240c6657afe85304a78
yude <i@yude.jp>
2024-09-05 20:18:39 +0900

    fix: Remove duplicate Tabler definition

--- structs/bedrock.go
+++ structs/bedrock.go
@@ -1,11 +1,9 @@
+package structs
+
 type Bedrock struct {
 	ID   int    `gorm:"column:id;primaryKey;autoIncrement"json:"id"`
 	FUID string `gorm:"column:fuid;type:char(36);unique;not null"json:"fuid"`
 	XUID string `gorm:"column:xuid;type:char(36);unique;not null"json:"xuid"`
-}
-
-type Tabler interface {
-	TableName() string
 }
 
 func (Bedrock) TableName() string {
---
+++ structs/global.go
@@ -0,0 +1,5 @@
+package structs
+
+type Tabler interface {
+	TableName() string
+}
